d45b44215a5db0f16cc3c6d798cc16881317d4e5
Yati Sagade <yati.sagade@gmail.com>
2015-10-01 17:40:05 +0200

    Clean up main

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,22 @@
-extern crate rand;
 extern crate exprolution;
+
 use std::env;
-use rand::Rng;
-use exprolution::expr;
-use exprolution::genetic::{Chromosome,ga};
+use exprolution::genetic;
 
 
 fn main() {
     let args = env::args().collect::<Vec<_>>();
+
     if args.len() < 2 {
         println!("Need a number");
         return;
     }
-    let num = args[1].parse::<f64>().unwrap();
-    match ga(500, num) {
+
+    let num = args[1].parse::<f64>().expect(
+        &format!("{} is not a valid number", args[1])
+    );
+
+    match genetic::ga(500, num) {
         (ngens, Some(ref c)) => {
             println!("Found a solution in {} generations:", ngens);
             println!("\t{}", c.decode());
